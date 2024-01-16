63e6d7e0664f80b39cd6462a105c45a12fbaf59a
siddharthsahu123 <153170514+siddharthsahu123@users.noreply.github.com>
2024-01-16 18:09:31 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3)// we are using nodejs , not browser
+
+console.log(3
+    +
+    3)//code readibility should be high
+    let name="siddharth"
+    let age=20
+    let isLoggedin=false
+
+
+//number=>2 to power 53
+//bigint
+//string=>""
+// Boolean=>true/false
+// null=>satndalone value
+// undefined=>
+// Symbol=>unique
+
+
+// Object
+
+console.log(typeof null);
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=12572
+let accountEmail="SID@GMAIL.C=OM"
+var accountPassword="12112"
+accountCity="jaipur"
+// accountId=2 not aalowed
+accountEmail="SID@GMAIL.C=OM"
+accountPassword="12112"
+accoun0tCity="jaipur"
+let accountState;
+
+
+console.table([accountEmail,accountId,accountPassword,accoun0tCity,accountState])
+/*
+PREFER not to use var
+because of issue in block scope and functional scope
+*/ 
