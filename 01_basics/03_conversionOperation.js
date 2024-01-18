e6af271eea5d1d4a450ef06fb085fe991d25cf04
siddharthsahu123 <153170514+siddharthsahu123@users.noreply.github.com>
2024-01-18 09:45:14 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score ="33dcf"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33"=>33
+// "33abc"=>NaN
+// TRUE=>1,FALSE=>0
+
+let isLoggedin=1
+let booleanisLoggedin=Boolean(isLoggedin)
+console.log(booleanisLoggedin);
+
+// 1=>true;0=>false
+// ""=>false;"siddharth"=>true
+someNumber=33
+ let stringNumber=String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+
