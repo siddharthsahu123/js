bb3a6c3c99bd6276617c8ce591b81221fd4bb787
siddharthsahu123 <153170514+siddharthsahu123@users.noreply.github.com>
2024-01-18 14:37:32 +0000

    js

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,32 @@
+// primitive data types
+
+// 7:strings,number,boolean,null,undefined,BigInt,Symbol
+
+const scoreValue=100.3
+
+const isLoggoedin=null
+let userEmail;//undefined
+
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id==anotherId);
+
+const BigNumber=276372677682n//for bigint
+
+
+
+// REFERENCE(NON_primitive)
+
+// Array,objects,function
+    const num=["1","21","212"]
+    let myObj= {
+        name:"siddharth",
+        age:"20",
+    }
+
+const myFunction=function(params) {
+    console.log("Hello World");
+}
+
+
