dfb7f7aaab60e9a64a2b6479bb13c52aa6b1f1bd
siddharthsahu123 <153170514+siddharthsahu123@users.noreply.github.com>
2024-01-19 12:09:01 +0000

    maths and num

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,28 @@
+// const score=400
+// console.log(score);
+
+// const balance= new  Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const value=100.31231
+// console.log(value.toPrecision(5));
+
+// const hundred=10000000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+
+// ************************************MATHS*******************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+console.log(Math.random);// provides value between 0 and 1
+console.log(Math.floor((Math.random()*10)+1));
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min +1))+min);
+
